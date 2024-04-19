cf129d63bdd362a4c56adfac814cf51c0b66cb36
Min Htut <gmin69620@gmail.com>
2024-04-19 11:13:54 +0630

    Add

---
+++ MHDotNetCore.Consoleapp/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data.SqlClient;
+
+Console.WriteLine("Hello, World!");
+
+SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+stringBuilder.DataSource = ".";
+stringBuilder.InitialCatalog = "MHDotNetCore.ConsoleApp";
+stringBuilder.UserID = "sa";
+stringBuilder.Password = "sa@123";
+SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
+connection.Open();
+connection.Close(); 
+
+Console.ReadKey();
